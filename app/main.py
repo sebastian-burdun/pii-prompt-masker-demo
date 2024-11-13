b1609680727fe970676ade74e58b55472bcf002d
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-13 13:37:32 +0100

    "Rebrand" code (names matter!)

--- app/main.py
+++ app/main.py
@@ -8,53 +8,52 @@ import scrubadub, scrubadub_address, scrubadub_stanford
 nltk.download("punkt_tab")
 
 
-class ObfuscationMappingBuilder(scrubadub.post_processors.PostProcessor):
-    name = "obfuscation_mapping_builder"
-    obfuscation_mapping = {}
+class PIIMaskMapBuilder(scrubadub.post_processors.PostProcessor):
+    name = "pii_mask_map_builder"
+    mapping = {}
 
-    def process_filth(self, filth_list):
-        for filth in filth_list:
-            self.obfuscation_mapping[filth.replacement_string] = filth.text
-        return filth_list
+    def process_filth(self, item_list):
+        for item in item_list:
+            self.mapping[item.replacement_string] = item.text
+        return item_list
 
 
 app = FastAPI()
-obfuscator = scrubadub.Scrubber(
+pii_masker = scrubadub.Scrubber(
     post_processor_list=[
         scrubadub.post_processors.FilthReplacer(
             include_hash=True, hash_salt="example", hash_length=5
         ),
-        ObfuscationMappingBuilder(),
+        PIIMaskMapBuilder(),
     ]
 )
-obfuscator.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
-obfuscator.add_detector(scrubadub_address.detectors.AddressDetector)
+pii_masker.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
+pii_masker.add_detector(scrubadub_address.detectors.AddressDetector)
 
 
-class ObfuscationData(BaseModel):
+class QuestionData(BaseModel):
     prompt: str
     context: str
 
 
-class ResponseData(BaseModel):
+class AnswerData(BaseModel):
     response: str
 
 
-@app.post("/generate-answer", response_model = ResponseData)
-def generate_answer(obfuscation_data: ObfuscationData) -> Any:
-    original_text = " ".join([obfuscation_data.prompt, obfuscation_data.context])
-    llm_request_text = obfuscator.clean(original_text)
-    obfuscation_mapping = next(
+@app.post("/generate-answer", response_model = AnswerData)
+def generate_answer(question_data: QuestionData) -> Any:
+    original_text = " ".join([question_data.prompt, question_data.context])
+    question = pii_masker.clean(original_text)
+    pii_mask_map = next(
         processor
-        for processor in obfuscator._post_processors
-        if isinstance(processor, ObfuscationMappingBuilder)
-    ).obfuscation_mapping
+        for processor in pii_masker._post_processors
+        if isinstance(processor, PIIMaskMapBuilder)
+    ).mapping
 
-    llm_response_text = llm_request_text
-    for obfuscated_item, clarified_item in obfuscation_mapping.items():
-        llm_response_text = llm_response_text.replace(obfuscated_item, clarified_item)
+    answer = question
+    for masked_item, unmasked_item in pii_mask_map.items():
+        answer = answer.replace(masked_item, unmasked_item)
 
     return {
-        "random_data": "random_data",
-        "response": llm_response_text,
+        "response": answer,
     }
