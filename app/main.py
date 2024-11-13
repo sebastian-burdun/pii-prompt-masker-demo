e236b950b2e853c18d3e3c7208db33d9c0ee18a5
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-13 10:29:16 +0100

    Create test setup

--- app/main.py
+++ app/main.py
@@ -2,28 +2,32 @@ from fastapi import FastAPI
 import nltk
 import scrubadub, scrubadub_address, scrubadub_stanford
 
-nltk.download('punkt_tab')
+nltk.download("punkt_tab")
 
 app = FastAPI()
-scrubber = scrubadub.Scrubber()
-scrubber.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
-scrubber.add_detector(scrubadub_address.detectors.AddressDetector)
+obfuscator = scrubadub.Scrubber()
+obfuscator.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
+obfuscator.add_detector(scrubadub_address.detectors.AddressDetector)
+
+TEST_TEXT = """
+John's phone number is 555-123-4567. He lives at 123 Maple Street
+in San Francisco, and his email is john.doe@example.com.
+"""
+
 
 @app.get("/")
 def read_root():
-    # obfuscation_mapping = {}
+    obfuscation_mapping = {}
 
-    # def create_obfuscation_mapping(dirty_text, clean_text, **kwargs):
-    #     mapping[dirty_text] = clean_text
+    def create_obfuscation_mapping(original_text, obfuscated_text, **kwargs):
+        obfuscation_mapping[original_text] = obfuscated_text
 
-    # text = (
-    #     "John's phone number is 555-123-4567. He lives at 123 Maple Street "
-    #     "in San Francisco, and his email is john.doe@example.com."
-    # )
-    # scrubbed_text = scrubber.clean(
-    #     text,
-    #     callback=create_obfuscation_mapping,
-    # )
-    # scrubber.clean("My name is John and I work at the United Nations in Geneva")
-    import ipdb; ipdb.set_trace()
-    return {"Hello": "World"}
+    obfuscated_text = obfuscator.clean(
+        TEST_TEXT,
+        callback=create_obfuscation_mapping,
+    )
+    return {
+        "input_text": TEST_TEXT,
+        "output_text": obfuscated_text,
+        "mapping": obfuscation_mapping,
+    }
