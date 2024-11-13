c9d91d564ac86f07854c00e0a370556c2fedd7e5
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-13 11:28:40 +0100

    Add simple hashing for generating masked tokens

--- app/main.py
+++ app/main.py
@@ -5,7 +5,13 @@ import scrubadub, scrubadub_address, scrubadub_stanford
 nltk.download("punkt_tab")
 
 app = FastAPI()
-obfuscator = scrubadub.Scrubber()
+obfuscator = scrubadub.Scrubber(
+    post_processor_list=[
+        scrubadub.post_processors.FilthReplacer(
+            include_hash=True, hash_salt="example", hash_length=5
+        )
+    ]
+)
 obfuscator.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
 obfuscator.add_detector(scrubadub_address.detectors.AddressDetector)
 
