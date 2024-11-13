8b1ab08ba99ab658bf4cdc62d43d76b67e1339dd
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-13 12:27:54 +0100

    Revert "Attempt recreate obfuscator on each request"
    This reverts commit 8bc9b351f8c6377dbe03eb37bc17dac4957ee53a.

--- app/main.py
+++ app/main.py
@@ -16,33 +16,27 @@ class ObfuscationMappingBuilder(scrubadub.post_processors.PostProcessor):
 
 
 app = FastAPI()
+obfuscator = scrubadub.Scrubber(
+    post_processor_list=[
+        scrubadub.post_processors.FilthReplacer(
+            include_hash=True, hash_salt="example", hash_length=5
+        ),
+        ObfuscationMappingBuilder(),
+    ]
+)
+obfuscator.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
+obfuscator.add_detector(scrubadub_address.detectors.AddressDetector)
 
 TEST_TEXT = """
 John's phone number is 555-123-4567. He lives at 123 Maple Street
 in San Francisco, and his email is john.doe@example.com.
 """
 
-def initialize_obfuscator():
-    obfuscator = scrubadub.Scrubber(
-        post_processor_list=[
-            scrubadub.post_processors.FilthReplacer(
-                include_hash=True, hash_salt="example", hash_length=5
-            ),
-            ObfuscationMappingBuilder(),
-        ]
-    )
-    obfuscator.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
-    obfuscator.add_detector(scrubadub_address.detectors.AddressDetector)
-
-    return obfuscator
-
 
 @app.get("/")
 def read_root():
-    obfuscator = initialize_obfuscator()
-    obfuscated_text = obfuscator.clean(TEST_TEXT)
     request = TEST_TEXT
-    obfuscator = initialize_obfuscator()
+    obfuscated_text = obfuscator.clean(TEST_TEXT)
     obfuscated_text = obfuscator.clean("Carl")
     obfuscation_mapping = next(
         processor
