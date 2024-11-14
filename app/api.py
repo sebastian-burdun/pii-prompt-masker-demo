acb7959697b167f925883c30865f9fa514b89cbc
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-14 09:24:07 +0100

    Divide into modules

--- app/api.py
+++ app/api.py
@@ -1,34 +1,12 @@
 from typing import Any
 
 from fastapi import FastAPI
-import nltk
 from pydantic import BaseModel
-import scrubadub, scrubadub_address, scrubadub_stanford
 
-nltk.download("punkt_tab")
-
-
-class PIIMaskMapBuilder(scrubadub.post_processors.PostProcessor):
-    name = "pii_mask_map_builder"
-    mapping = {}
-
-    def process_filth(self, item_list):
-        for item in item_list:
-            self.mapping[item.replacement_string] = item.text
-        return item_list
+from pii_masker import pii_masker, PIIMaskMapBuilder
 
 
 app = FastAPI()
-pii_masker = scrubadub.Scrubber(
-    post_processor_list=[
-        scrubadub.post_processors.FilthReplacer(
-            include_hash=True, hash_salt="example", hash_length=5
-        ),
-        PIIMaskMapBuilder(),
-    ]
-)
-pii_masker.add_detector(scrubadub_stanford.detectors.StanfordEntityDetector)
-pii_masker.add_detector(scrubadub_address.detectors.AddressDetector)
 
 
 class QuestionData(BaseModel):
