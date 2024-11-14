3e2e431112d7d9963a0f696bf48d38e8204e5475
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-14 18:07:55 +0100

    Fix the source for ChatOpenAI class

--- app/llm_client.py
+++ app/llm_client.py
@@ -1,6 +1,6 @@
 from settings import MODEL_NAME, OPENAPI_KEY
 
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 
 llm_client = ChatOpenAI(openai_api_key=OPENAPI_KEY, model_name=MODEL_NAME)
