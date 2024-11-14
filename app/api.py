621a020ef4502d5fe242dc85baf57bbdc0424326
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-14 12:24:33 +0100

    Prepare setup for developing and testing OpenAI integration

--- app/api.py
+++ app/api.py
@@ -1,8 +1,11 @@
 from typing import Any
 
 from fastapi import FastAPI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from pydantic import BaseModel
 
+from llm_client import llm
 from pii_masker import pii_masker
 
 
@@ -22,7 +25,25 @@ class AnswerData(BaseModel):
 def generate_answer(question_data: QuestionData) -> Any:
     original_question = " ".join([question_data.prompt, question_data.context])
     masked_question = pii_masker.clean(original_question)
-    # LLM call
+    # # Define a simple prompt template
+    # template = PromptTemplate.from_template("Write a friendly introduction for {name}.")
+
+    # # Create a chain that links the prompt template and the LLM
+    # llm_chain = LLMChain(prompt=template, llm=llm)
+
+    # # Define input for the prompt
+    # input_data = {"name": "Alice"}
+
+    # # Execute the chain and print the result
+    # result = llm_chain.run(input_data)
+    # print(result)
+
+
+    # debug = os.getenv('DEBUG')
+
+    # print("Database URL:", database_url)
+    # print("Secret Key:", secret_key)
+    # print("Debug Mode:", debug)
     original_answer = masked_question
     unmasked_answer = pii_masker.unmask(original_answer)
     return {
