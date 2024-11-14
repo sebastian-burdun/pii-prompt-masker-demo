b3800faee6be83e05424071092386eae3836b31e
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-14 09:49:02 +0100

    Refactor code before hooking LLM

--- app/pii_masker.py
+++ app/pii_masker.py
@@ -1,3 +1,5 @@
+from typing import Any, List, NoReturn
+
 import nltk
 import scrubadub, scrubadub_address, scrubadub_stanford
 
@@ -6,15 +8,34 @@ nltk.download("punkt_tab")
 
 class PIIMaskMapBuilder(scrubadub.post_processors.PostProcessor):
     name = "pii_mask_map_builder"
-    mapping = {}
+    pii_mask_map = {}
 
-    def process_filth(self, item_list):
+    def process_filth(
+        self, item_list: List[scrubadub.filth.name.NameFilth]
+    ) -> List[scrubadub.filth.name.NameFilth]:
         for item in item_list:
-            self.mapping[item.replacement_string] = item.text
+            self.pii_mask_map[item.replacement_string] = item.text
         return item_list
 
 
-pii_masker = scrubadub.Scrubber(
+class PIIMasker(scrubadub.Scrubber):
+    def _initialize_pii_mask_map(self) -> NoReturn:
+        self.pii_mask_map = next(
+            processor
+            for processor in self._post_processors
+            if isinstance(processor, PIIMaskMapBuilder)
+        ).pii_mask_map
+
+    def unmask(self, masked_text: str) -> str:
+        self._initialize_pii_mask_map()
+        unmasked_text = masked_text
+        for masked_item, unmasked_item in self.pii_mask_map.items():
+            unmasked_text = unmasked_text.replace(masked_item, unmasked_item)
+
+        return unmasked_text
+
+
+pii_masker = PIIMasker(
     post_processor_list=[
         scrubadub.post_processors.FilthReplacer(
             include_hash=True, hash_salt="example", hash_length=5
