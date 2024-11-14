801b6be9868d4873b1b9040f8b058c0c5556f347
Sebastian Burdun <sebastian.burdun@gmail.com>
2024-11-14 17:16:03 +0100

    Fix question generation and add logging

--- app/api.py
+++ app/api.py
@@ -1,8 +1,7 @@
+import logging
 from typing import Any
 
 from fastapi import FastAPI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 from pydantic import BaseModel
 
 from llm_client import llm_client
@@ -11,6 +10,8 @@ from pii_masker import pii_masker
 
 app = FastAPI()
 
+
+logger = logging.getLogger('uvicorn.error')
 
 class QuestionData(BaseModel):
     prompt: str
@@ -21,18 +22,14 @@ class AnswerData(BaseModel):
     response: str
 
 
-@app.post("/generate-answer", response_model = AnswerData)
+@app.post("/generate-answer", response_model=AnswerData)
 def generate_answer(question_data: QuestionData) -> Any:
-    original_question = " ".join([question_data.prompt, question_data.context])
-    masked_question = pii_masker.clean(original_question)
-    masked_answer = llm_client.predict(masked_question)
-    unmasked_answer = pii_masker.unmask(masked_answer)
-    # llm = OpenAI(api_key=api_key, model_name="text-davinci-003", temperature=0.7)
-    # template = PromptTemplate.from_template("Write a friendly introduction for {name}.")
-    # llm_chain = LLMChain(prompt=template, llm=llm)
-    # input_data = {"name": "Alice"}
-    # result = llm_chain.run(input_data)
+    masked_question = pii_masker.clean(
+        " ".join([question_data.context, question_data.prompt])
+    )
+    logger.debug(f"Question sent to LLM: '{masked_question}'")
 
-    return {
-        "response": unmasked_answer,
-    }
+    masked_answer = llm_client.predict(masked_question)
+    logger.debug(f"Answer received from LLM: '{masked_answer}'")
+
+    return {"response": pii_masker.unmask(masked_answer)}
